c060e6a693567b199ce4872e9dadf26ab3438a3d
plehegar <plh@w3.org>
2026-02-02 20:26:33 -0500

    Entirely remove fetchJSON

--- test/lib/fetch.test.js
+++ test/lib/fetch.test.js
@@ -1,19 +1,19 @@
 import { strict as assert } from 'node:assert/strict';
 import { suite, test } from 'node:test';
-import * as fetch_utils from '../../lib/fetch.js';
+import { json } from '../../lib/fetch.js';
 
 suite('lib/utils', () => {
   
   suite('json', () => {
     test('group exists', async () => {
-      const f1 = await fetch_utils.json("https://api.w3.org/groups/other/tag");
+      const f1 = await json("https://api.w3.org/groups/other/tag");
       assert.deepEqual(f1.id, 34270);
     });
     test('group does not exist', () => {
-      assert.rejects(fetch_utils.json("https://api.w3.org/groups/other/unknown"));
+      assert.rejects(json("https://api.w3.org/groups/other/unknown"));
     });
     test('not even JSON', () => {
-      assert.rejects(fetch_utils.json("https://www.w3.org/"));
+      assert.rejects(json("https://www.w3.org/"));
     });
   });
   
